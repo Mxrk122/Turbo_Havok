755ebf890a616d8656c44d3fef576085bb482713
José Rodrigo Barrera <70623939+bar20807@users.noreply.github.com>
2024-02-29 22:03:48 -0600

    fix controller

--- Assets/Final Form Studio/Script/controler.cs
+++ Assets/Final Form Studio/Script/controler.cs
@@ -1,22 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class controler : MonoBehaviour
+public class ControladorCarro : MonoBehaviour
 {
-    public WheelCollider Rueda1;
-    public WheelCollider Rueda2;
-    public int velocity = 150;
-    // Start is called before the first frame update
-    void Start()
-    {
+    public float velocidad = 5f; // Velocidad de movimiento del carro
 
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        Rueda1.motorTorque = velocity * Input.GetAxis("Vertical");
-        Rueda2.motorTorque = velocity * Input.GetAxis("Vertical");
+        // Obtener las entradas de teclado o joystick
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        // Calcular el vector de dirección basado en las entradas
+        Vector3 direccion = new Vector3(horizontal, 0f, vertical).normalized;
+
+        // Mover el carro en la dirección calculada
+        MoverCarro(direccion);
+    }
+
+    void MoverCarro(Vector3 direccion)
+    {
+        // Obtener el componente Rigidbody adjunto al carro
+        Rigidbody rb = GetComponent<Rigidbody>();
+
+        // Calcular el vector de movimiento
+        Vector3 movimiento = direccion * velocidad * Time.deltaTime;
+
+        //Se transforma el collider del carro para rotarlo según si va a la derecha o izquierda
+        if (direccion.x > 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 90, 0);
+        }
+        else if (direccion.x < 0)
+        {
+            transform.rotation = Quaternion.Euler(0, -90, 0);
+        }
+
+        // Aplicar el movimiento al Rigidbody
+        rb.MovePosition(rb.position + movimiento);
     }
 }
